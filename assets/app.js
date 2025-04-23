a249f6be3d78c01ac3047f286e8252bafe722c5d
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2025-04-23 23:19:03 +0200

    Mise en place du CRUD des évènements et du style des formulaires et des selects

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,4 @@ import './bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles/app.css';
+import 'tom-select/dist/css/tom-select.bootstrap5.css'
