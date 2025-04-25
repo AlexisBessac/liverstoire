f027a760a8863d72f349b9d649c56e376f01e0f8
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2025-04-25 17:03:03 +0200

    Mise en place de la frise chronologique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\EventsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Events;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EventsRepository $eventsRepository): Response
     {
+        $events = $eventsRepository->findBy([], ['chronos' => 'ASC']);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'events' => $events,
+        ]);
+    }
+
+    #[Route('/book/{id}', name: 'app_events_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Events $bevents): Response
+    {
+        // Vérifie si le livre existe
+        if (!$bevents) {
+            throw $this->createNotFoundException("Il n'existe pas de description de cet évéènement");
+        }
+
+        return $this->render('home/show.html.twig', [
+            'events' => $bevents,
         ]);
     }
 }
